0de6045f27f1c7cf0881750c66eb648c5446ab21
Kamil <kkamilstasiakk@gmail.com>
2016-12-31 13:49:27 +0100

    gotowe

--- src/pl/angry/bbb/Main.java
+++ src/pl/angry/bbb/Main.java
@@ -3,15 +3,13 @@ package pl.angry.bbb;
 import dk.i1.diameter.*;
 import dk.i1.diameter.node.*;
 
+import java.io.IOException;
+
 
 /**
  * A simple Credit-control server that accepts and grants everything
  */
-class cc_test_server extends NodeManager {
-    cc_test_server(NodeSettings node_settings) {
-        super(node_settings);
-    }
-
+class Server {
 
     //Kamil
     public static final void main(String args[]) throws Exception {
@@ -29,7 +27,19 @@ class cc_test_server extends NodeManager {
             port = 3868;
 
         DiameterServer ds = new DiameterServer();
-        ds.addAAServer(host_id,realm,port);
+        DiameterAAServer aaServer = ds.addAAServer(host_id,realm,port);
+        aaServer.addUser("user@example.pl", "aaa", "assdasfffsaf");
+        aaServer.setClusterAddress("224.0.0.1");
+
+        System.out.println("Hit enter to terminate server");
+
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        aaServer.stop(50); //Stop but allow 50ms graceful shutdown
     }
 
 
